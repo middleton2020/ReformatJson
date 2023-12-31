f06d1edbd851552da43cb39898af2faba5a4c84c
Colin Middleton <microsoft@medievalresearch.co.uk>
2023-12-31 18:38:08 +0000

    Add the 'lost' code for ReformatJson.

--- ReformatJson/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ReformatJson
-{
-    public class Class1
-    {
-    }
-}
---
+++ ReformatJson/ReformatJson.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CM.ReformatJson.Processing
+{
+    public class ResformatJson
+    {
+        public string MakeReadable(string inpOriginalJson)
+        {
+            try
+            {
+                string readableJson = MakeReadable(inpOriginalJson, "    ", ":");
+                return readableJson;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public string MakeReadable(string inpOriginalJson, string inpIndentChars)
+        {
+            try
+            {
+                string readableJson = MakeReadable(inpOriginalJson, inpIndentChars, ":");
+                return readableJson;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public string MakeReadable(string inpOriginalJson, string inpIndentChars, string inpColonChar)
+        {
+            try
+            {
+                string readableJson = inpOriginalJson;
+
+                return readableJson;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
